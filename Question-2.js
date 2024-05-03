f32d5959dd26dc7bb4e6942f2894c2a33dea3cce
Md. Shahadat Hasan Sifat <100550955+Programmer-md-sifat@users.noreply.github.com>
2024-05-03 21:37:02 +0600

    Add files via upload

---
+++ Question-2.js
@@ -0,0 +1,13 @@
+// Question 2: Write a function maxOfTwo that takes two arguments, a and b, and returns the larger of the two numbers.
+
+
+function maxofTwo(a, b) {
+
+    return a > b ? a : b;
+}
+  
+console.log(maxofTwo(15, 7)); // Output: 15
+console.log(maxofTwo(10, 11)); // Output: 11
+
+
+// My Journey Share With Github

eb7b8f3ae57809ed26d2eaaf9c750b02bf662140
Md. Shahadat Hasan Sifat <100550955+Programmer-md-sifat@users.noreply.github.com>
2024-05-03 21:22:58 +0600

    Add files via upload

---
+++ Question-1.js
@@ -0,0 +1,13 @@
+// Question 1: Write a function addTwoNumbers that takes two arguments, a and b, and returns their sum.
+
+
+function addTwoNumbers(a, b) {
+
+    return a + b;
+}
+
+console.log(addTwoNumbers(3, 5)); // Output: 8
+console.log(addTwoNumbers(11, -5)); // Output: 8
+
+
+// My Journey Share With Github

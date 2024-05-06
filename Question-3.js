f550eb6baffbc3cbd7b1c796f1c4bb49659a59c6
Md. Shahadat Hasan Sifat <100550955+Programmer-md-sifat@users.noreply.github.com>
2024-05-06 10:06:50 +0600

    Add files via upload

---
+++ Question-3.js
@@ -0,0 +1,13 @@
+// Question 3: Write a function reverseString that takes a string str and returns the reversed version of the string.
+
+
+function reverseString(str) {
+
+    return str.split('').reverse().join('');
+}
+
+console.log(reverseString("Hello")); // Output: "olleH"
+console.log(reverseString("Javascript"));  // Output: "tpircSavaJ"
+
+
+// My Journey Share With Github

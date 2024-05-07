948d7942cde119f4faf12447d5f99babb8b2dcab
Md. Shahadat Hasan Sifat <100550955+Programmer-md-sifat@users.noreply.github.com>
2024-05-07 12:34:01 +0600

    Add files via upload

---
+++ Question-4.js
@@ -0,0 +1,13 @@
+// Question 4: Write a function isPositive that takes a number n and returns true if it's positive (greater than zero), and false otherwise.
+
+
+function isPositive(n) {
+
+    return n > 0;
+}
+
+console.log(isPositive(5)); // Output: true
+console.log(isPositive(-3)); // Output: false
+
+
+// My Journey Share With Github
